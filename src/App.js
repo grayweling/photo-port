e0ff78e5e2c6cead2475e4c9a549458e2d2550b1
grayweling <jy6228@yahoo.com>
2022-05-08 10:42:29 -0500

    updated to snapshot

--- src/App.js
+++ src/App.js
@@ -7,21 +7,12 @@ function App() {
   const [categories] = useState([
     {
       name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects'
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
     },
-    {
-      name: 'portraits',
-      description: 'Portraits of people in my life'
-    },
-    {
-      name: 'food',
-      description: 'Delicious delicacies'
-    },
-    {
-      name: 'landscape',
-      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
-    }
-  ])
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
@@ -29,14 +20,12 @@ function App() {
     <div>
       <Nav
         categories={categories}
-        currentCategory={currentCategory}
         setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
       ></Nav>
       <main>
-        <div>
-          <Gallery></Gallery>
-          <About></About>
-        </div>        
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
